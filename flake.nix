eaed6c91a8aa4d573d85e067e93afb84503d6886
princedimond <princedimond@gmail.com>
2026-08-31 00:32:21 -0500

    added catppuccin to flake

--- flake.nix
+++ flake.nix
@@ -4,6 +4,10 @@
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
 
   inputs = {
+    catppuccin = {
+      url = "github:catppuccin/nix/release-26.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     flake-file.url = "github:vic/flake-file";
     flake-parts.url = "github:hercules-ci/flake-parts";
     home-manager = {
