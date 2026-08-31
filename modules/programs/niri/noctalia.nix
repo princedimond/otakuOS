cf672c72043f8cf446134da8bf41deec51ee676a
princedimond <princedimond@gmail.com>
2026-08-31 00:16:39 -0500

    added noctalia to niri and system desktop template

--- modules/programs/niri/noctalia.nix
+++ modules/programs/niri/noctalia.nix
@@ -1,8 +1,13 @@
-{ ... }:
+{ inputs,... }:
 {
   flake.modules.nixos.niri =
-    { ... }:
+    { pkgs, ... }:
     {
+      services.power-profiles-daemon.enable = true;
+      services.upower.enable = true;
+      environment.systemPackages = [
+        inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
+      ];
     };
 
 }
