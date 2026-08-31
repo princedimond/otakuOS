00cbf2c7cf0ee71a3e1c645592f530bd089e891e
princedimond <princedimond@gmail.com>
2026-08-31 01:04:55 -0500

    added ly as login manager for niri

---
+++ modules/programs/niri/login.nix
@@ -0,0 +1,25 @@
+{ ... }:
+{
+  flake.modules.nixos.niri =
+    { pkgs, ... }:
+    {
+      environmment.systempackages = with pkgs; [
+        ly
+      ];
+
+      services.displayManager.ly = {
+        enable = true;
+        settings = {
+          animation = "matrix";
+          bigclock = true;
+          bg = "0x00000000";
+          fg = "0x0000FFFF";
+          border_fg = "0x00FF0000";
+          error_fg = "0x00FF0000";
+          clock_color = "#800080";
+          min_uid = 1000;
+          max_uid = 2000;
+        };
+      };
+    };
+}
