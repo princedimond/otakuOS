5849046c036569813ce284cbea5b11595b828f3c
princedimond <princedimond@gmail.com>
2026-08-30 22:47:49 -0500

    adjust filesystems for host pd-19kdh72

--- modules/hosts/PD-19KDH72/filesystems.nix
+++ modules/hosts/PD-19KDH72/filesystems.nix
@@ -1,19 +1,27 @@
 { ... }:
 {
   flake.modules.nixos.PD-19KDH72 = {
-    fileSystems = {
-      "/" = {
-        device = "/dev/disk/by-uuid/[UUID]";
-        fsType = "btrfs";
-      };
-      "/boot" = {
-        device = "/dev/disk/by-uuid/[UUID]";
-        fsType = "vfat";
-        options = [
-          "fmask=0077"
-          "dmask=0077"
-        ];
-      };
-    };
+    fileSystems."/" =
+        { device = "/dev/disk/by-uuid/0ca27e97-faca-41b9-8554-fd3d348096b6";
+          fsType = "btrfs";
+        };
+
+      fileSystems."/home" =
+        { device = "/dev/disk/by-uuid/0ca27e97-faca-41b9-8554-fd3d348096b6";
+          fsType = "btrfs";
+          options = [ "subvol=home" ];
+        };
+
+      fileSystems."/nix" =
+        { device = "/dev/disk/by-uuid/0ca27e97-faca-41b9-8554-fd3d348096b6";
+          fsType = "btrfs";
+          options = [ "subvol=nix" ];
+        };
+
+      fileSystems."/boot" =
+        { device = "/dev/disk/by-uuid/2388-D4A7";
+          fsType = "vfat";
+          options = [ "fmask=0077" "dmask=0077" ];
+        };
   };
 }
