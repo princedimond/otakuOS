635d011ca83dbf95621502e33100b61a04112b67
princedimond <princedimond@gmail.com>
2026-08-31 00:19:12 -0500

    added cache for noctalia in niri module

--- modules/programs/niri/noctalia.nix
+++ modules/programs/niri/noctalia.nix
@@ -3,6 +3,10 @@
   flake.modules.nixos.niri =
     { pkgs, ... }:
     {
+      nix.settings = {
+        extra-substituters = [ "https://noctalia.cachix.org" ];
+        extra-trusted-public-keys = [ "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=" ];
+      };
       services.power-profiles-daemon.enable = true;
       services.upower.enable = true;
       environment.systemPackages = [
