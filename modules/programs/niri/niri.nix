f791669999077abc51b6e2117bb7c57fe05e14e7
princedimond <princedimond@gmail.com>
2026-09-01 22:44:27 -0500

    added xwayland satellite to niri config for x11 fallback

--- modules/programs/niri/niri.nix
+++ modules/programs/niri/niri.nix
@@ -1,10 +1,13 @@
-{ inputs,... }:
+{ inputs, ... }:
 {
   flake.modules.nixos.niri =
-    { ... }:
+    { pkgs, ... }:
     {
       imports = [
-       inputs.niri.nixosModules.niri
+        inputs.niri.nixosModules.niri
+      ];
+      environment.systemPackages = with pkgs; [
+        xwayland-satellite
       ];
       programs.niri.enable = true;
     };
