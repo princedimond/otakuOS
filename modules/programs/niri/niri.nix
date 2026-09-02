51a7dfbc6d2b6d21bcd6bec8762fcc0cd71cfe9c
princedimond <princedimond@gmail.com>
2026-09-01 22:50:49 -0500

    added portals to niri config

--- modules/programs/niri/niri.nix
+++ modules/programs/niri/niri.nix
@@ -9,6 +9,17 @@
       environment.systemPackages = with pkgs; [
         xwayland-satellite
       ];
+      xdg.portal = {
+        enable = true;
+        xdgOpenUsePortal = true;
+        extraPortals = [
+          pkgs.xdg-desktop-portal-gtk
+          pkgs.xdg-desktop-portal-gnome
+        ];
+        config = {
+          common.default = [ "gnome" ];
+        };
+      };
       programs.niri.enable = true;
     };
 
