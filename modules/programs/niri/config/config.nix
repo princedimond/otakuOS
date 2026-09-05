42daa1fa30c4e1daf008134e0d32594dd9d1b55b
cacarl <princedimond@gmail.com>
2026-09-05 01:40:14 -0500

    added noctalia config via .toml file import

--- modules/programs/niri/config/config.nix
+++ modules/programs/niri/config/config.nix
@@ -2,5 +2,6 @@
 {
   flake.modules.homeManager.niri = {
     xdg.configFile."niri/config.kdl".source = ./config.kdl;
+    xdg.configFile."noctalia/noctalia-config.toml".source = ./noctalia-config.toml;
   };
 }
