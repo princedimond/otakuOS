d7a1531eaaa17137ca701b54cf63a42f52817c82
cacarl <princedimond@gmail.com>
2026-09-06 00:17:32 -0500

    formatted noctalia.nix

--- modules/programs/niri/noctalia.nix
+++ modules/programs/niri/noctalia.nix
@@ -1,11 +1,13 @@
-{ inputs,... }:
+{ inputs, ... }:
 {
   flake.modules.nixos.niri =
     { pkgs, ... }:
     {
       nix.settings = {
         extra-substituters = [ "https://noctalia.cachix.org" ];
-        extra-trusted-public-keys = [ "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=" ];
+        extra-trusted-public-keys = [
+          "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+        ];
       };
       services.power-profiles-daemon.enable = true;
       services.upower.enable = true;
@@ -13,5 +15,4 @@
         inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
       ];
     };
-
 }
