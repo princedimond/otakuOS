4e117928b97550bd8f1c47ad2962fd874e4c9ca9
cacarl <princedimond@gmail.com>
2026-09-21 22:09:37 -0500

    separated thunar into its own module to add plugins and services

--- modules/programs/princedimond/princedimond-gui/thunar.nix
+++ modules/programs/princedimond/princedimond-gui/thunar.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ ... }:
 {
   flake.modules.nixos.princedimond-gui =
     { pkgs, ... }:
@@ -7,22 +7,19 @@
         thunar
         ffmpegthumbnailer # needed to preview images and videos
       ];
+      programs.thunar = {
+        enable = true;
+        plugins = with pkgs; [
+          thunar-archive-plugin
+          thunar-volman
+          thunar-vcs-plugin
+          thunar-shares-plugin
+          thunar-media-tags-plugin
+        ];
+      };
+      services.gvfs.enable = true;
+      services.tumbler.enable = true;
+      services.udisks2.enable = true;
+      programs.xfconf.enable = true;
     };
-
-  flake.modules.homeManager.princedimond-gui = {
-    programs.thunar = {
-      enable = true;
-      plugins = with pkgs; [
-        thunar-archive-plugin
-        thunar-volman
-        thunar-vcs-plugin
-        thunar-shares-plugin
-        thunar-media-tags-plugin
-      ];
-    };
-    services.gvfs.enable = true;
-    services.tumbler.enable = true;
-    services.udisks2.enable = true;
-    programs.xconf.enable = true;
-  };
 }
